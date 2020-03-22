ab0e1ec1e4617e234ee2412132cd19614c7ffae3
ydaydada@gmail.com <rekuill@hotmail.com>
2020-03-22 12:50:30 +0900

    git add utils

---
+++ utils.js
@@ -0,0 +1,11 @@
+module.exports = {
+    double: (input)=>{
+        return input * 2
+    },
+    triple: (input)=>{
+        return input * 3
+    },
+    quattro: (input)=>{
+        return input * 4
+    }
+}
