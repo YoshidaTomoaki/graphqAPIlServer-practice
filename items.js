9292b8b48c657f87576da9a0645133be0315e1a9
ydaydada@gmail.com <rekuill@hotmail.com>
2020-03-22 12:50:12 +0900

    git add datastore(json)

---
+++ items.js
@@ -0,0 +1,20 @@
+exports.items = [
+    {
+        id: 1,
+        name: 'ギター',
+        price: 10000,
+        category: 'Rock'
+    },
+    {
+        id: 2,
+        name: 'ドラム',
+        price: 50000,
+        category: 'Jazz'
+    },
+    {
+        id: 3,
+        name: 'ベース',
+        price: 30000,
+        category: 'Pops'
+    }
+];
---
+++ users.js
@@ -0,0 +1,68 @@
+exports.users = [
+    {
+        id: 1,
+        name: '吉田',
+        age: 28,
+        createdAt: '2019-04-05 01:35:25.000',
+        admin: true,
+        role: 'ギタリスト',
+        item: {
+            id: 1,
+            name: 'ギター',
+            price: 10000,
+            category: 'Rock'
+        }
+    },
+    {
+        id: 2,
+        name: '星野',
+        age: 27,
+        createdAt: '2019-04-05 01:35:25.000',
+        admin: true,
+        role: 'ドラマー',
+        item: {
+            id: 2,
+            name: 'ドラム',
+            price: 10000,
+            category: 'Rock'
+        }
+    },
+    {
+        id: 3,
+        name: '太田',
+        age: 26,
+        createdAt: '2019-04-05 01:35:25.000',
+        admin: true,
+        role: 'ベーシスト',
+        item: {
+            id: 3,
+            name: 'ベース',
+            price: 10000,
+            category: 'Rock'
+        }
+    },
+    {
+        id: 4,
+        name: '赤澤',
+        age: 26,
+        createdAt: '2019-04-05 01:35:25.000',
+        normal: true,
+        role: '平民'
+    },
+    {
+        id: 5,
+        name: '和田',
+        age: 26,
+        createdAt: '2019-04-05 01:35:25.000',
+        normal: true,
+        role: 'ギタリスト'
+    },
+    {
+        id: 6,
+        name: '小柳',
+        age: 26,
+        createdAt: '2019-04-05 01:35:25.000',
+        normal: true,
+        role: 'ギタリスト'
+    }
+];
